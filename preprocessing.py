bbbfcf50ecd69221632be485bf88bd9f3328a03d
Jean-Marcel Herzog <dev@jmherzog.de>
2022-05-14 13:13:25 +0200

    Update preprocessing.py
    Implementation of Canny edge detection and simple binary threshold.

--- preprocessing.py
+++ preprocessing.py
@@ -19,19 +19,34 @@ def update_original_frame(image: np.ndarray):
     image_8bit = cv.normalize(image, None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
     image_8bit_f = image_8bit.astype('float64')
     # image_8bit_f = np.where(image_8bit_f > 250, 0, image_8bit_f)
-    cv.imshow("Live Bild", cv.resize(image_8bit, (1024, 1024)))
+    cv.imshow("Live Image", cv.resize(image_8bit, (1024, 1024)))
 
     if not diff_set:
         diff_img = image_8bit_f.copy()
-        cv.imshow("REFERENZ BILD", cv.resize(diff_img.astype('uint8'), (1024, 1024)))
+        cv.imshow("Reference Image", cv.resize(diff_img.astype('uint8'), (1024, 1024)))
         diff_set = True
     elif frame_counter == 1:
         img = np.where(abs(image_8bit_f - diff_img) > 80, image_8bit_f, 0)
         blur_img = cv.blur(img.astype('uint8'), (10, 10))
+
+        # Sobel Y-Axis
         gray_y = cv.Sobel(blur_img, cv.CV_8U, 0, 1, ksize=3, scale=1, delta=0, borderType=cv.BORDER_DEFAULT)
+
+        # Apply Adaptive Threshold
         thresh1 = cv.adaptiveThreshold(gray_y, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 6)
-        cv.imshow("Y-Kanten", cv.resize(thresh1, (1024, 1024)))
-        cv.imshow("Differenz Bild", cv.resize(img.astype('uint8'), (1024, 1024)))
+
+        # Apply fixed threshold
+        thresh2 = cv.threshold(gray_y, thresh=50, maxval=255, type=cv.THRESH_BINARY)
+
+        # Apply canny edge detection
+        canny = cv.Canny(gray_y, threshold1=50, threshold2=150, edges=None, apertureSize=3, L2gradient=False)
+
+        cv.imshow("Difference Image", cv.resize(img.astype('uint8'), (1024, 1024)))
+        cv.imshow("Sobel Y", cv.resize(gray_y, (1024, 1024)))
+        cv.imshow("Adaptive Threshold", cv.resize(thresh1, (1024, 1024)))
+        cv.imshow("Threshold", cv.resize(thresh2, (1024, 1024)))
+        cv.imshow("Canny", cv.resize(canny, (1024, 1024)))
+
         frame_counter = 0
     else:
         frame_counter += 1
