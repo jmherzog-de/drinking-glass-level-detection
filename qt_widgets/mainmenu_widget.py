80e7e6a5e265b570c6d3a71c278c7613bb53cdee
Jean-Marcel Herzog <dev@jmherzog.de>
2022-05-26 14:23:17 +0200

    Create mainmenu_widget.py

---
+++ qt_widgets/mainmenu_widget.py
@@ -0,0 +1,7 @@
+from PySide6.QtWidgets import QWidget, QGroupBox, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QFileDialog
+
+
+class MainMenuWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
