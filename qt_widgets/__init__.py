a89afe816ef40f74d69749dcfd71c9db54b30caa
Jean-Marcel Herzog <dev@jmherzog.de>
2022-05-26 14:23:21 +0200

    Update __init__.py

--- qt_widgets/__init__.py
+++ qt_widgets/__init__.py
@@ -1,3 +1,8 @@
 from .histogram_widget import HistogramWidget
 from .image_widget import ImageWidget
 from .camera_controls_widget import CameraControlsWidget
+from .video_controls_widget import VideoControlsWidget
+from .roi_widget import ROIWidget
+from .mainmenu_widget import MainMenuWidget
+from .navigation_widget import NavigationWidget
+
