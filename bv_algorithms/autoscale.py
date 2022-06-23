fe29edc599322c6bec33083622425c08c2c54e4b
Jean Marcel Herzog <dev@jmherzog.de>
2022-06-23 10:57:55 +0200

    Add docstrings

--- bv_algorithms/autoscale.py
+++ bv_algorithms/autoscale.py
@@ -19,14 +19,31 @@ lib.apply.argtypes = [BV_HelperHandle, c_int_array]
 
 
 class AutoscaleImage(object):
+    """
+    Binding class to C++ autoscaling methods.
+
+    This class contains binding to generate a Look-Up table and create an auto-scaled image.
+    Note: Works with 16-bit image.
+    """
 
     def __init__(self):
         self.instance = lib.createInstance()
 
-    def create_lookup_table(self, t_min: int, t_max: int):
+    def create_lookup_table(self, t_min: int, t_max: int) -> None:
+        """
+        Create Look-Up table with min and max pixel value.
+        :param t_min: Minimum pixel value.
+        :param t_max: Maximum pixel value.
+        :return: None
+        """
         lib.createLookUpTable(self.instance, t_min, t_max)
 
     def autoscale(self, image: np.ndarray):
+        """
+        Perform autoscaling for any input image.
+        :param image: Input grayscale 16-bit image
+        :return: Scaled 16-bit image.
+        """
         scaled_image = image.copy()
         lib.createLookUpTable(self.instance, image.min(), 32750)
         lib.apply(self.instance, scaled_image)
