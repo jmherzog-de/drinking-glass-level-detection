7f50d9f513731c9f828206e63e6b99f5f64ee5dc
Jean Marcel Herzog <dev@jmherzog.de>
2022-06-27 21:05:02 +0200

    Changed wording and add docstrings

--- qt_widgets/glas_detection_widget.py
+++ qt_widgets/glas_detection_widget.py
@@ -6,52 +6,52 @@ import bv_algorithms as bv
 from .image_widget import ImageWidget
 
 
-class GlasDetectionTabWidget(QWidget):
+class GlassDetectionTabWidget(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        self.glas_detector = bv.GlassDetection()
+        self.glass_detector = bv.GlassDetection()
 
-        self.glas_detected: bool = False
-        self.detected_glas_frames = []
+        self.glass_detected: bool = False
+        self.detected_glass_frames = []
         self.cycles_counter: int = 0
         self.ref_pos = (0, 0, 0, 0)  # x1, y1, x2, y2
 
         self.central_layout = QHBoxLayout(self)
         self.central_layout.setObjectName(u"central_layout")
 
-        self.groupbox_glas_extraction = QGroupBox()
-        self.groupbox_glas_extraction.setObjectName(u"groupbox_glas_extraction")
-        self.groupbox_glas_extraction.setTitle(u"Glas Detection")
-        self.groupbox_glas_extraction_layout = QHBoxLayout(self.groupbox_glas_extraction)
-        self.groupbox_glas_extraction_layout.setObjectName(u"groupbox_glas_extraction_layout")
+        self.groupbox_glass_extraction = QGroupBox()
+        self.groupbox_glass_extraction.setObjectName(u"groupbox_glass_extraction")
+        self.groupbox_glass_extraction.setTitle(u"Glas Detection")
+        self.groupbox_glass_extraction_layout = QHBoxLayout(self.groupbox_glass_extraction)
+        self.groupbox_glass_extraction_layout.setObjectName(u"groupbox_glass_extraction_layout")
 
-        self.glas_extraction_image_widget = ImageWidget()
-        self.glas_extraction_image_widget.setObjectName(u"glas_extraction_image_widget")
-        self.groupbox_glas_extraction_layout.addWidget(self.glas_extraction_image_widget)
+        self.glass_extraction_image_widget = ImageWidget()
+        self.glass_extraction_image_widget.setObjectName(u"glass_extraction_image_widget")
+        self.groupbox_glass_extraction_layout.addWidget(self.glass_extraction_image_widget)
 
-        self.glas_stencil_widget = ImageWidget()
-        self.glas_stencil_widget.setObjectName(u"glas_stencil_image_widget")
-        self.groupbox_glas_extraction_layout.addWidget(self.glas_stencil_widget)
+        self.glass_stencil_widget = ImageWidget()
+        self.glass_stencil_widget.setObjectName(u"glass_stencil_image_widget")
+        self.groupbox_glass_extraction_layout.addWidget(self.glass_stencil_widget)
 
-        self.central_layout.addWidget(self.groupbox_glas_extraction)
+        self.central_layout.addWidget(self.groupbox_glass_extraction)
 
     def update_image(self, frame: np.ndarray):
         """
-        Glas detection has finished, when the program detects the glas on 30 cycles on the same spot
+        Glass detection has finished, when the program detects the glass on 30 cycles on the same spot
         :param frame:
         :return:
         """
 
-        frame = self.glas_detector.detect(frame)
+        frame = self.glass_detector.detect(frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
-        x, y, x2, y2 = self.glas_detector.estimated_glas()
-        if self.glas_detector.state():
-            self.glas_stencil_widget.update_image(self.glas_detector.get_glas_stencil())
+        x, y, x2, y2 = self.glass_detector.estimated_glass()
+        if self.glass_detector.state():
+            self.glass_stencil_widget.update_image(self.glass_detector.get_glass_stencil())
             cv2.rectangle(frame, (x, y), (x2, y2), (0, 255, 0), 3)
         elif x is not None:
             cv2.rectangle(frame, (x, y), (x2, y2), (255, 255, 0), 3)
 
-        self.glas_extraction_image_widget.update_image(frame)
+        self.glass_extraction_image_widget.update_image(frame)
